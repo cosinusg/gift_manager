3c9e7897bde061d4f5d7cf84a61af249e92c8889
cosinusg <nosti2adamus@gmail.com>
2022-10-09 21:10:01 +0300

    1 commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:gift_manager/presentation/login/view/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
+
