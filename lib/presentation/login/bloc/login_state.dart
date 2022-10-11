0f31e0d27b0fc808cb67e8c30c30533869d0b277
cosinusg <nosti2adamus@gmail.com>
2022-10-11 21:30:00 +0300

    Added login errors

--- lib/presentation/login/bloc/login_state.dart
+++ lib/presentation/login/bloc/login_state.dart
@@ -5,42 +5,60 @@ part of 'login_bloc.dart';
 
 class LoginState extends Equatable {
   final String email;
+  final EmailError emailError;
   final String password;
+  final PasswordError passwordError;
   final bool emailValid;
   final bool passwordValid;
   final bool authenticated;
 
   const LoginState({
     required this.email,
+    required this.emailError,
     required this.password,
+    required this.passwordError,
     required this.emailValid,
     required this.passwordValid,
     required this.authenticated,
   });
 
-  factory LoginState.initial() => const LoginState(email: '', password: '', emailValid: false, passwordValid: false, authenticated: false,);
+  factory LoginState.initial() => const LoginState(
+        email: '',
+        emailError: EmailError.noError,
+        password: '',
+        passwordError: PasswordError.noError,
+        emailValid: false,
+        passwordValid: false,
+        authenticated: false,
+      );
 
   LoginState copyWith({
     final String? email,
-  final String? password,
-  final bool? emailValid,
-  final bool? passwordValid,
-  final bool? authenticated,
+    final EmailError? emailError,
+    final String? password,
+    final PasswordError? passwordError,
+    final bool? emailValid,
+    final bool? passwordValid,
+    final bool? authenticated,
   }) {
     return LoginState(
-      email: email ?? this.email, 
-      password: password ?? this.password, 
-      emailValid: emailValid ?? this.emailValid, 
-      passwordValid: passwordValid ?? this.passwordValid, 
+      email: email ?? this.email,
+      emailError: emailError ?? this.emailError,
+      password: password ?? this.password,
+      passwordError: passwordError ?? this.passwordError,
+      emailValid: emailValid ?? this.emailValid,
+      passwordValid: passwordValid ?? this.passwordValid,
       authenticated: authenticated ?? this.authenticated,
-      );
+    );
   }
 
   @override
   List<Object> get props {
     return [
       email,
+      emailError,
       password,
+      passwordError,
       emailValid,
       passwordValid,
       authenticated,
